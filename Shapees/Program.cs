faa1eeba9f36f20b83c4476da79d913d93b8dbbd
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 15:41:51 -0300

    main

--- Shapees/Program.cs
+++ Shapees/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Shapees.Entities;
+using Shapees.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace Shapees
 {
@@ -6,7 +10,40 @@ namespace Shapees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Shapee> list = new List<Shapee>();
+
+            Console.Write("Enter the number of shapes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Shape #{i} data:");
+                Console.Write("Rectangle or Circle (r/c)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Color (Black/Blue/Red): ");
+                Color color = Enum.Parse<Color>(Console.ReadLine());
+                if (ch == 'r')
+                {
+                    Console.Write("Width: ");
+                    double width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    Console.Write("Height: ");
+                    double height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    Console.Write("Radius: ");
+                    double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Circle(radius, color));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("SHAPE AREAS:");
+            foreach (Shapee shape in list)
+            {
+                Console.WriteLine(shape.Area().ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
