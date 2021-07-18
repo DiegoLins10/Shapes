6c02dd8560bd223d3a698cdc9371f1113d470427
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 15:42:32 -0300

    sub classes

---
+++ Shapees/Entities/Circle.cs
@@ -0,0 +1,22 @@
+﻿using Shapees.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapees.Entities
+{
+    class Circle : Shapee
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius, Color color) : base (color)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * (Radius * Radius);
+        }
+    }
+}
---
+++ Shapees/Entities/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using Shapees.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapees.Entities
+{
+    class Rectangle : Shapee
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color) : base(color)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+    }
+}
