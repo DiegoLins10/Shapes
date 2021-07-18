fffb3dab722d2b16f95c87606afa02d053e0d1fd
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 15:14:13 -0300

    Add project files.

---
+++ Shapees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shapees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
