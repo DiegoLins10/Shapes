f7a6ea55c29d35a5b5923dffc7ebbc27204c1005
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 15:42:13 -0300

    super class shapee

---
+++ Shapees/Entities/Shapee.cs
@@ -0,0 +1,20 @@
+﻿using Shapees.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapees.Entities
+{
+    abstract class Shapee
+    {
+        public Color Color { get; set; }
+
+
+        public Shapee(Color color)
+        {
+            Color = color;
+        }
+
+        public abstract double Area();
+    }
+}
