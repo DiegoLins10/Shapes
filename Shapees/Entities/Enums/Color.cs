94fb4a3383f2e64b4f7c164ed1fe54d5af6dae4d
diegolins10 <diegofernandeslins@gmail.com>
2021-07-18 15:42:02 -0300

    enums color

---
+++ Shapees/Entities/Enums/Color.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapees.Entities.Enums
+{
+    enum Color
+    {
+        Black,
+        Blue,
+        Red
+    }
+}
